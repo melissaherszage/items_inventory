57a34177820a03c0526ff766cc3f21ef17aab94c
Melissa Herszage <mherszage@tapi.la>
2024-10-26 00:48:09 -0300

    arreglo los tests

--- tests/test_get_dag_sql_files.py
+++ tests/test_get_dag_sql_files.py
@@ -1,19 +1,10 @@
 import pytest
+import os
 from python_files.lib.sql_folder import get_dag_sql_files
-
-def get_dag_sql_files(dag_folder: str = None ,
-                        dag_sub_folder: str = None):
-    
-    """
-    Agarra todos los archivos sql dentro de una carpeta, por default la del mismo nombre del dag.
-    Opcional tambien ponerle una subcarpte
-    :param dag_folder: carpeta donde estan los sql de ese DAG -> por default es el nombre del dag
-    :dag_sub_folder: subcarpeta dentro de la anterior - opcional
-    """
 
 def test_get_dag_sql_files_without_sub_folder():
     dag_folder = 'l2'
-    expected_path = 'dags/sql_files/l2'
+    expected_path = 'sql_files/l2'
     result = get_dag_sql_files(dag_folder=dag_folder)
     print(result)
-    assert result == expected_path
+    assert all(file.startswith(expected_path) for file in result), f"Resulting files do not start with {expected_path}"
